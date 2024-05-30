0b5772e10cbfbf01d84cc0371638ec83b7b1f514
Vector-ops <sumith2827@gmail.com>
2024-05-30 17:30:09 +0530

    fix: replace ' with &apos;

--- app/contact/page.jsx
+++ app/contact/page.jsx
@@ -30,10 +30,10 @@ const Contact = () => {
 					<div className="xl:w-[54%] order-2 xl:order-none">
 						<form className="flex flex-col gap-6 p-10 bg-[#27272c] rounded-xl">
 							<h3 className="text-4xl text-accent">
-								Let's work together
+								Let&apos;s work together
 							</h3>
 							<p className="text-white/60">
-								Have a project you'd like to discuss?
+								Have a project you&apos;d like to discuss?
 							</p>
 							<div className="grid grid-cols-1 md:grid-cols-2 gap-6">
 								<Input
